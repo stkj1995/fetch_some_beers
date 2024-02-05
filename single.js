1c0579ff9e4f4b302d411a2073fd476dc781f5f1
Sophie <soph1155@stud.kea.dk>
2024-02-05 14:13:13 +0100

    tilføj buzz øl

--- single.js
+++ single.js
@@ -14,3 +14,22 @@ function vis(data) {
   document.querySelector("p").textContent = data[0].description;
   document.querySelector("img").src = data[0].image_url;
 }
+
+
+//fra produkt.js
+
+// const urlParams = new URLSearchParams(window.location.search);
+// const id = urlParams.get("id");
+
+// fetch("https://kea-alt-del.dk/t7/api/products/" + id)
+//   .then((response) => response.json())
+//   .then((data) => showProduct(data));
+
+// function showProduct(product) {
+//   console.log(product);
+//   document.querySelector(".tilvalg h3").textContent = product.productdisplayname;
+//   document.querySelector(".tilvalg koen").textContent = product.gender;
+//   document.querySelector(".tilvalg overdel").textContent = product.subcategory;
+//   document.querySelector(".tilvalg brand").textContent = product.brandname;
+//   document.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
+// }
